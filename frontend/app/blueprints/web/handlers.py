149c5b1c77db2ca7c62c7fe2bd1aaecb5036ecd9
Joe <pogzyb@umich.edu>
2020-03-16 20:36:06 -0400

    Add item and index page updates using js

--- frontend/app/blueprints/web/handlers.py
+++ frontend/app/blueprints/web/handlers.py
@@ -1,15 +1,20 @@
 import logging
 from datetime import datetime
+import json
+import time
 
 from flask_login import current_user
+from flask_socketio import send, emit
 from flask import (
     Blueprint,
     jsonify,
     render_template,
     redirect,
-    request
+    request,
+    url_for
 )
 
+from app import socketio
 from app.blueprints.web import web
 from app.aws_utils.sqs import send_to_queue
 from app.aws_utils.ddb import (
@@ -40,7 +45,7 @@ def scrape_term():
     }
     insert_record(item=payload)
     send_to_queue(item=payload)
-    return redirect('/')
+    return redirect(url_for('web.view_term', term=form.get('search-term')))
 
 
 @web.route('/view/<term>', methods=['GET'])
@@ -48,3 +53,17 @@ def view_term(term: str):
     wiki_record = get_single_record(term)
     data = format_query_result([wiki_record['Item']])
     return render_template('web/term.html', data=data)
+
+
+@socketio.on('view-page')
+def page_socket(term: str):
+    logger.info(f'Checking status for {term}...')
+    while True:
+        record = get_single_record(term)
+        data = format_query_result([record['Item']])[0]
+        if data.get('standing') == 'complete':
+            logger.info(f'Complete!')
+            break
+        time.sleep(1)
+
+    emit('status', {'message': 'Done!'})
